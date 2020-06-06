d2cf8fcbb87b5046d06742580d33dc6ee6ad7cd4
Marcelo Oliveira <Marcelo.Oliveira@wolterskluwer.com>
2020-06-05 21:05:36 -0300

    Alterações na controller WeatherForecast.

--- EFCore.WebAPI/Controllers/WeatherForecastController.cs
+++ EFCore.WebAPI/Controllers/WeatherForecastController.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using EFCore.Domain.Entities;
+using EFCore.Infra.Data.Context;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Linq;
 
 namespace EFCore.WebAPI.Controllers
 {
@@ -17,23 +16,130 @@ namespace EFCore.WebAPI.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        public readonly HeroiContext _context;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="context"></param>
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, HeroiContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public ActionResult Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            //var listHerois = _context.Herois.ToList();
+            var listHerois = (from heroi in _context.Herois select heroi).ToList();
+
+            return Ok(listHerois);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            var heroi = new Heroi() { Nome = "Spider-Man" };
+
+            _context.Herois.Add(heroi);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("Atualizar/{nome}")]
+        public ActionResult Get(string nomeHeroi)
+        {
+            var heroi = _context.Herois
+                                .Where(h => h.Id == 4)
+                                .FirstOrDefault();
+
+            heroi.Nome = "Homem Aranha";
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("filtro/{nome}")]
+        public ActionResult GetFiltro(string nome)
+        {
+            var listHerois = _context.Herois
+                                     .Where(h => h.Nome.Contains(nome))
+                                     .ToList();
+
+            //var listHerois = (from heroi in _context.Herois
+            //                  where heroi.Nome.Contains(nome)
+            //                  select heroi).ToList();
+
+            return Ok(listHerois);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("AddRange")]
+        public ActionResult GetAddRange()
+        {
+            _context.AddRange(
+                new Heroi { Nome = "Capitão América" },
+                new Heroi { Nome = "Doutor Estranho" },
+                new Heroi { Nome = "Pantera Negra" },
+                new Heroi { Nome = "Viúva Negra" },
+                new Heroi { Nome = "Hulk" },
+                new Heroi { Nome = "Gavião Arqueiro" },
+                new Heroi { Nome = "Capitã Marvel" }
+            );
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpGet("Delete/{id}")]
+        public void Delete(int id)
+        {
+            var heroi = _context.Herois
+                                .Where(h => h.Id == id)
+                                .Single();
+
+            _context.Herois.Remove(heroi);
+            _context.SaveChanges();
         }
     }
 }
